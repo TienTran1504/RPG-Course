f896c8f93337d2edb3acf6b7ba713ffc4158da76
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-10-26 19:11:49 +0700

    element effect: lightning

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -44,8 +44,8 @@ public class CharacterStats : MonoBehaviour
     private float igniteDamageCooldown = .3f;
     private float igniteDamageTimer;
     private int igniteDamage;
-
-
+    [SerializeField] private GameObject shockStrikePrefab;
+    private int shockDamage;
     public int currentHealth;
 
     public System.Action onHealthChanged;
@@ -147,6 +147,9 @@ public class CharacterStats : MonoBehaviour
         if(canApplyIgnite){
             _targetStats.SetupIgniteDamage(Mathf.RoundToInt(_fireDamage * .2f));
         }
+        if(canApplyShock){
+            _targetStats.SetupShockStrikeDamage(Mathf.RoundToInt(_lightingDamage * .1f));
+        }
 
         _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
 
@@ -161,17 +164,19 @@ public class CharacterStats : MonoBehaviour
    }
 
     public void SetupIgniteDamage(int _damage) => igniteDamage = _damage;
+    public void SetupShockStrikeDamage(int _damage) => shockDamage = _damage;
 
    public void ApplyAilments(bool _ignite, bool _chill, bool _shock){
-        if (isIgnited || isChilled || isShocked) return;
-
-        if(_ignite){
+        bool canApplyIgnite = !isIgnited && !isChilled && !isShocked;
+        bool canApplyChill = !isIgnited && !isChilled && !isShocked;
+        bool canApplyShock = !isIgnited && !isChilled;
+        if(_ignite && canApplyIgnite){
             isIgnited = _ignite;
             ignitedTimer = ailmentsDuaration;
 
             fx.IgniteFxFor(ailmentsDuaration);
         }
-        if (_chill)
+        if (_chill && canApplyChill)
         {
             isChilled = _chill;
             chilledTimer = ailmentsDuaration;
@@ -180,14 +185,63 @@ public class CharacterStats : MonoBehaviour
             GetComponent<Entity>().SlowEntityBy(slowPercentage,ailmentsDuaration);
             fx.ChillFxFor(ailmentsDuaration);
         }
-        if (_shock)
+        if (_shock && canApplyShock)
         {
-            isShocked = _shock;
-            shockedTimer = ailmentsDuaration;
+            if (!isShocked)
+            {
+                ApplyShock(_shock);
+            }
+            else
+            {
+                if (GetComponent<Player>() != null) return;
 
-            fx.ShockFxFor(ailmentsDuaration);
+
+                HitNearestTargetWithShockStrike();
+
+
+            }
         }
    }
+    public void ApplyShock(bool _shock)
+    {
+        if (isShocked) return;
+
+        shockedTimer = ailmentsDuaration;
+        isShocked = _shock;
+        fx.ShockFxFor(ailmentsDuaration);
+    }
+    private void HitNearestTargetWithShockStrike()
+    {
+        //find closest target enemy
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 25);
+
+        float closestDistance = Mathf.Infinity;
+        Transform closestEnemy = null;
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null && Vector2.Distance(transform.position, hit.transform.position) > 1)
+            {
+                float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+                    closestEnemy = hit.transform;
+                }
+            }
+            if (closestEnemy == null)
+            {
+                closestEnemy = transform;
+            }
+        }
+
+
+        if (closestEnemy != null)
+        {
+            GameObject newShockStrike = Instantiate(shockStrikePrefab, transform.position, Quaternion.identity);
+
+            newShockStrike.GetComponent<ShockStrike_Controller>().Setup(shockDamage, closestEnemy.GetComponent<CharacterStats>());
+        }
+    }
     public virtual void TakeDamage(int _damage){
         DecreaseHealthBy(_damage);
         Debug.Log(_damage);
