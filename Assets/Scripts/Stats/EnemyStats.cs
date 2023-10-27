7c7c11a9a8674313f4cc791fc236d434601e49ea
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-10-27 18:36:15 +0700

    Increase enemy attribute by level

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -5,10 +5,50 @@ using UnityEngine;
 public class EnemyStats : CharacterStats
 {
     private Enemy enemy;
+    [Header("Level deatils")]
+    [SerializeField] private int level =1;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float percentageModifier = .4f;
+
     protected override void Start()
     {
+        ApplyLevelModifer();
+
         base.Start();
         enemy = GetComponent<Enemy>();
+    }
+
+    private void ApplyLevelModifer()
+    {
+        Modify(strength);
+        Modify(agility);
+        Modify(intelligence);
+        Modify(vitality);
+
+        Modify(damage);
+        Modify(critChance);
+        Modify(critPower);
+
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicResistance);
+
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(lightingDamage);
+
+        
+    }
+
+    private void Modify(Stat _stat){
+        for (int i = 1; i < level; i++)
+        {
+            float modifier = _stat.GetValue() * percentageModifier;
+
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)
