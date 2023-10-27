e094cdd8beb7ec964dff6a28f5a460faa1e1ad4d
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-10-27 15:02:16 +0700

    Inventory UI & Logic pickup item in inventory

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -8,6 +8,10 @@ public class Inventory : MonoBehaviour
     public List<InventoryItem> inventoryItems;
     public Dictionary<ItemData, InventoryItem> inventoryDictionary;
 
+    [Header("Inventory UI")]
+    [SerializeField] private Transform inventorySlotParent;
+    private UI_ItemSlot[] itemSlot;
+
     private void Awake() {
         if(instance == null){
             instance = this;
@@ -18,6 +22,13 @@ public class Inventory : MonoBehaviour
     private void Start(){
         inventoryItems = new List<InventoryItem>();
         inventoryDictionary = new Dictionary<ItemData, InventoryItem>();
+        itemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
+    }
+
+    private void UpdateSlotUI() {
+        for (int i =0; i<inventoryItems.Count;i++){
+            itemSlot[i].UpdateSlot(inventoryItems[i]);
+        }
     }
 
 
@@ -30,6 +41,7 @@ public class Inventory : MonoBehaviour
             inventoryItems.Add(newItem);
             inventoryDictionary.Add(_item, newItem);
         }
+        UpdateSlotUI();
     }
 
     public void RemoveItem(ItemData _item){
@@ -42,6 +54,7 @@ public class Inventory : MonoBehaviour
                 value.RemoveStack();
             }
         }
+        UpdateSlotUI();
     }
 
 }
--- Assets/ItemObject.cs
+++ Assets/ItemObject.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class ItemObject : MonoBehaviour
 {
-    private SpriteRenderer sr;
-
     [SerializeField] private ItemData itemData;
 
-    private void Start(){
-        sr = GetComponent<SpriteRenderer>();
-
-        sr.sprite = itemData.icon;
+    private void OnValidate() {
+        GetComponent<SpriteRenderer>().sprite = itemData.icon;
+        gameObject.name = "Item object - " + itemData.itemName;
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.GetComponent<Player>() != null){
-            // Debug.Log("Picked up item " + itemData.itemName);
+            Debug.Log("Picked up item " + itemData.itemName);
             Inventory.instance.AddItem(itemData);
             Destroy(gameObject);
         }
