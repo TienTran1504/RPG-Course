ce52e39d861ec178c7a82e0435ffb742a9b7431e
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-11-06 18:06:29 +0700

    Death Bringer enemy

--- Assets/Scripts/UI_Game/UI_HealthBar.cs
+++ Assets/Scripts/UI_Game/UI_HealthBar.cs
@@ -6,19 +6,15 @@ using UnityEngine.UI;
 public class UI_HealthBar
  : MonoBehaviour
 {
-    private Entity entity;
-    private CharacterStats myStats;
+    private Entity entity => GetComponentInParent<Entity>();
+    private CharacterStats myStats => GetComponentInParent<CharacterStats>();
     private RectTransform myTransform;
     private Slider slider;
     private void Start(){
         myTransform = GetComponent<RectTransform>();
-        entity = GetComponentInParent<Entity>();
         slider = GetComponentInChildren<Slider>();
-        myStats = GetComponentInParent<CharacterStats>();
 
-        entity.onFlipped += FlipUI;
 
-        myStats.onHealthChanged += UpdateHealthUI;
 
         UpdateHealthUI();
     }
@@ -30,13 +26,26 @@ public class UI_HealthBar
         slider.maxValue = myStats.GetMaxHealthValue();
         slider.value = myStats.currentHealth;
     }
-    private void FlipUI() {
-        myTransform.Rotate(0,180,0);
+
+
+    private void OnEnable()
+    {
+        entity.onFlipped += FlipUI;
+        myStats.onHealthChanged += UpdateHealthUI;
     }
 
 
     private void OnDisable() {
-        entity.onFlipped -= FlipUI;
-        myStats.onHealthChanged -= UpdateHealthUI;
+        if (entity != null) { 
+            entity.onFlipped -= FlipUI;
+        }
+        if (myStats != null)
+        {
+            myStats.onHealthChanged -= UpdateHealthUI;
+        }
+    }
+
+        private void FlipUI() {
+        myTransform.Rotate(0,180,0);
     }
 }
